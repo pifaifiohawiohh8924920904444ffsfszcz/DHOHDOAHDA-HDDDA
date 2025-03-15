22a79a8c6b19ee4d74f177bdd7a87136954dfb76
wrealaero <clanwarobi@gmail.com>
2025-03-14 23:23:26 -0500

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -22,7 +22,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/pifaifiohawiohh8924920904444ffsfszcz/DHOHDOAHDA-HDDDA/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/pifaifiohawiohh8924920904444ffsfszcz//DHOHDOAHDA-HDDDA/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -460,21 +460,21 @@ run(function()
 					local oldchannel = textChatService.ChatInputBarConfiguration.TargetTextChannel
 					local newchannel = cloneref(game:GetService('RobloxReplicatedStorage')).ExperienceChat.WhisperChat:InvokeServer(v.UserId)
 					if newchannel then
-						newchannel:SendAsync('; I USING DHOHDOAHDA-HDDDA VXPE')
+						newchannel:SendAsync('; I USING QP VXPE')
 					end
 					textChatService.ChatInputBarConfiguration.TargetTextChannel = oldchannel
 					textChatService.ChannelTabsConfiguration.Enabled = false
 				elseif replicatedStorage:FindFirstChild('DefaultChatSystemChatEvents') then
-					replicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer('/w '..v.Name..' ; I USING DHOHDOAHDA-HDDDA VXPE', 'All')
+					replicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer('/w '..v.Name..' ; I USING QP VXPE', 'All')
 				end
 			end
 		end
 	end
 
 	function whitelist:process(msg, plr)
-		if plr == lplr and msg == '; I USING DHOHDOAHDA-HDDDA VXPE' then return true end
+		if plr == lplr and msg == '; I USING QP VXPE' then return true end
 
-		if self.localprio > 0 and not self.said[plr.Name] and msg == '; I USING DHOHDOAHDA-HDDDA VXPE' and plr ~= lplr then
+		if self.localprio > 0 and not self.said[plr.Name] and msg == '; I USING QP VXPE' and plr ~= lplr then
 			self.said[plr.Name] = true
 			notif('Vape', plr.Name..' is using vape!', 60)
 			self.customtags[plr.Name] = {{
@@ -552,7 +552,7 @@ run(function()
 				vape:Clean(exp:FindFirstChild('RCTScrollContentView', true).ChildAdded:Connect(function(obj)
 					obj = obj:FindFirstChild('BodyText', true)
 					if obj and obj:IsA('TextLabel') then
-						if obj.Text:find('; I USING DHOHDOAHDA-HDDDA VXPE') then
+						if obj.Text:find('; I USING QP VXPE') then
 							obj.Parent.Parent.Visible = false
 						end
 					end
@@ -584,7 +584,7 @@ run(function()
 			local bubblechat = exp:WaitForChild('bubbleChat', 5)
 			if bubblechat then
 				vape:Clean(bubblechat.DescendantAdded:Connect(function(newbubble)
-					if newbubble:IsA('TextLabel') and newbubble.Text:find('; I USING DHOHDOAHDA-HDDDA VXPE') then
+					if newbubble:IsA('TextLabel') and newbubble.Text:find('; I USING QP VXPE') then
 						newbubble.Parent.Parent.Visible = false
 					end
 				end))
@@ -595,12 +595,12 @@ run(function()
 	function whitelist:update(first)
 		local suc = pcall(function()
 			local _, subbed = pcall(function()
-				return game:HttpGet('https://github.com/pifaifiohawiohh8924920904444ffsfszcz/whitelists/tree/main')
+				return game:HttpGet('https://github.com/wrealaero/whitelists/tree/main')
 			end)
 			local commit = subbed:find('currentOid')
 			commit = commit and subbed:sub(commit + 13, commit + 52) or nil
 			commit = commit and #commit == 40 and commit or 'main'
-			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/pifaifiohawiohh8924920904444ffsfszcz/whitelists/'..commit..'//whitelist.json', true)
+			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/wrealaero/whitelists/'..commit..'/t.json', true)
 		end)
 		if not suc or not hash or not whitelist.get then return true end
 		whitelist.loaded = true
@@ -680,17 +680,17 @@ run(function()
 	end
 
 	whitelist.commands = {
-		byfron = function()
-			while wait() do
-				pcall(function()
-					for i,v in game:GetDescendants() do
-						if v:IsA("RemoteEvent") and not string.find(v.Name:lower(),"lobby") and not string.find(v.Name:lower(),"teleport") then
-							v:FireServer()
-						end
-					end
-				end)
-			end
-		end,
+		-- byfron = function()
+		-- 	while wait() do
+		-- 		pcall(function()
+		-- 			for i,v in game:GetDescendants() do
+		-- 				if v:IsA("RemoteEvent") and not string.find(v.Name:lower(),"lobby") and not string.find(v.Name:lower(),"teleport") then
+		-- 					v:FireServer()
+		-- 				end
+		-- 			end
+		-- 		end)
+		-- 	end
+		-- end,
 		crash = function()
 			task.spawn(function()
 				repeat
@@ -739,9 +739,9 @@ run(function()
 		reveal = function()
 			task.delay(0.1, function()
 				if textChatService.ChatVersion == Enum.ChatVersion.TextChatService then
-					textChatService.ChatInputBarConfiguration.TargetTextChannel:SendAsync('I AM USING THE DHOHDOAHDA-HDDDA VXPE | d i s c o r d . g g / i c i c l e')
+					textChatService.ChatInputBarConfiguration.TargetTextChannel:SendAsync('I AM USING THE QP VXPE | d i s c o r d . g g / U 3 f Z a c B A p D')
 				else
-					replicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer('I AM USING THE DHOHDOAHDA-HDDDA VXPE | d i s c o r d . g g / i c i c l e', 'All')
+					replicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer('I AM USING THE QP VXPE | d i s c o r d . g g / U 3 f Z a c B A p D', 'All')
 				end
 			end)
 		end,
